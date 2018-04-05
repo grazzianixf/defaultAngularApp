3594a570a9f509f9e9d27508c208848113c98211
Grazziani Xavier Flor <grazziani.xavier@dataprev.gov.br>
2018-04-05 09:58:24 -0300

    removendo lixo do header e footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -2,28 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  templateUrl: './footer.component.html'
 })
 
 export class FooterComponent implements OnInit {
-  public links: any[] = [
-    {
-      name: 'GitHub',
-      url: 'https://github.com/tarlepp/angular2-firebase-material-demo',
-      icon: 'web',
-    },
-    {
-      name: 'Issues',
-      url: 'https://github.com/tarlepp/angular2-firebase-material-demo/issues',
-      icon: 'bug_report',
-    },
-    {
-      name: 'Tarmo Leppänen',
-      url: 'https://github.com/tarlepp',
-      icon: 'person',
-    },
-  ];
 
   constructor() { }
 
