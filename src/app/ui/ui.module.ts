26a281c8d2336e5735285d108900f501026bab0b
Grazziani Xavier Flor <grazziani.xavier@dataprev.gov.br>
2018-04-06 18:09:18 -0300

    Inserido o material design, no entanto com problema pra exibir um menu

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialDesignModule } from './material-design/material-design.module';
+
+@NgModule({
+  imports: [
+    CommonModule, MaterialDesignModule
+  ],
+  declarations: []
+})
+export class UiModule { }
