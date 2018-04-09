7dc9f2a93e73327e1a0df7be61db2318104db006
Grazziani Xavier <grazzianixf@gmail.com>
2018-04-08 22:27:24 -0300

    Ajustes de layout

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,9 +8,9 @@ import { Router } from '@angular/router';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private router: Router) {
+  constructor() {
     console.log('AboutComponent constructor');    
-  }  
+  }
 
   ngOnInit() {
   }
--- src/app/ui/material-design/material-design.module.ts
+++ src/app/ui/material-design/material-design.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatMenuModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import { MatMenuModule, MatIconModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule, 
-    MatMenuModule, MatIconModule, 
+    MatMenuModule, MatIconModule, MatSidenavModule,
     NoopAnimationsModule
   ],
   declarations: [],
-  exports: [ MatMenuModule, MatIconModule, MatToolbarModule ]
+  exports: [ MatMenuModule, MatIconModule, MatToolbarModule, MatSidenavModule ]
 })
 export class MaterialDesignModule { }
