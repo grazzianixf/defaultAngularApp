31f24fff29f9c74baa1e712ab6687334afc1c4d6
Grazziani Xavier Flor <grazziani.xavier@dataprev.gov.br>
2018-04-09 17:46:27 -0300

    Ajuste para trocar menu do header por MatSideNav

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 ## TODO
 
-* MD
+* Material Design
   * Centralizar spinner de processando (TodoComponent)
   * Mensagens no estilo vermelho
-* Componentizar menu
+* Menu
+  * Componentizar menu (hoje está dentro do app.component.html)
+  * Ocultar itens de acordo com permissão
 * Implementar Services por componente para consultar dados no Firebase
 * Implementar uso de models
 * README em layout padrão
 * AUTH
   * roles
+* Cadastro de usuários
--- src/app/ui/material-design/material-design.module.ts
+++ src/app/ui/material-design/material-design.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatMenuModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatProgressSpinnerModule, MatSnackBarModule } from '@angular/material';
+import { MatMenuModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatProgressSpinnerModule, MatSnackBarModule,
+          MatListModule } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule, 
-    MatMenuModule, MatIconModule, MatSidenavModule, MatSnackBarModule,
+    MatMenuModule, MatIconModule, MatSidenavModule, MatSnackBarModule, MatListModule,
     NoopAnimationsModule
   ],
   declarations: [],
-  exports: [ MatMenuModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatProgressSpinnerModule ]
+  exports: [ MatMenuModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatProgressSpinnerModule, MatListModule ]
 })
 export class MaterialDesignModule { }
